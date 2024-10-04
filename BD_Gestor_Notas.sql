8fae998bcc2c84b769b3876f4e32be3ab3f222a7
rodrey <rodreyjosue@gmail.com>
2024-10-04 12:06:18 -0600

    cambio de campo

--- BD_Gestor_Notas.sql
+++ BD_Gestor_Notas.sql
@@ -5,7 +5,7 @@ CREATE TABLE notas (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     estudiante VARCHAR (225) NOT NULL,
     actividades INT NOT NULL CHECK (actividades <= 35),
-    primerParcial INT NOT NULL CHECK (primerParcial <= 15),
+    primer_Parcial INT NOT NULL CHECK (primer_Parcial <= 15),
     segundoParcial INT NOT NULL CHECK (segundoParcial <= 15),
     examenFinal INT NOT NULL CHECK (examenFinal <= 35),
     notaFinal INT NOT NULL
